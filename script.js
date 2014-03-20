1d5ac39c7ae9363a4afe360e62505f75abb423da
dingocat <wizomagicland@gmail.com>
2014-03-20 08:19:34 -0700

    Bug Fixes
    Fixed minor bugs.

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
+	$("#text1").fadeIn("fast");
+	$("#text2").fadeOut("fast");
+	$("#text3").fadeOut("fast");
+	
 	$("#link1").click(function() {
-	$("#text1").fadeOut("fast", function()	{
+		$("#text1").fadeIn("fast");
+		$("#text2").fadeOut("fast");
+		$("#text3").fadeOut("fast");
+	});
+	$("#link2").click(function() {
+		$("#text1").fadeOut("fast");
+		$("#text2").fadeIn("fast");
+		$("#text3").fadeOut("fast");
+	});
+	$("#link3").click(function() {
+		$("#text1").fadeOut("fast");
+		$("#text2").fadeOut("fast");
+		$("#text3").fadeIn("fast");
 	});
 });
